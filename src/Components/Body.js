9adeb385d38915be3488df6a90d4bbb96f9a31ee
Sithara KS <sithara.ks@hitachids.com>
2025-07-11 05:50:01 +0530

    Swiggy API changes

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -15,7 +15,7 @@ const Body = () => {
     }, []);
 
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.951120136018453&lng=77.50633715842572&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.076008&lng=72.8776707&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
        
        setResList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
